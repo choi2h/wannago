7f83a911b5c4581d47a42b0a171d105d07046f77
choeihwa <chldlghk@gmail.com>
2025-06-27 14:43:34 +0900

    feat: add post insert and select

---
+++ wannago-server/src/main/java/com/wannago/post/dto/PostResponse.java
@@ -0,0 +1,17 @@
+package com.wannago.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PostResponse {
+    private Long id;
+    private String title;
+    private String author;
+    private String contents;
+    private boolean isPublic;
+}
