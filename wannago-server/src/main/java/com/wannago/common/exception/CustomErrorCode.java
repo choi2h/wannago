15caca2258fab95b645688188220b913d5210ef1
choeihwa <chldlghk@gmail.com>
2025-06-27 14:35:34 +0900

    env: add custom exception and handler

---
+++ wannago-server/src/main/java/com/wannago/common/exception/CustomErrorCode.java
@@ -0,0 +1,29 @@
+package com.wannago.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum CustomErrorCode {
+    // 회원 M
+    MEMBER_NOT_EXIST(HttpStatus.BAD_REQUEST, "M001", "존재하지 않는 회원입니다."),
+
+    // 게시글 P
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "요청하신 게시글을 찾을 수 없습니다.");
+
+
+    // 답변 C
+
+
+    // 질문 Q
+
+
+    // 답글 A
+
+    private final HttpStatus status;
+    private final String errorCode;
+    private final String message;
+}
+
