fba7a5fd9601c01518237c9b0d2a825a962dd528
kang1979 <kangbyeongch1979@gmail.com>
2025-07-02 15:18:01 +0900

    feat/"answer-create: Answer 수정"

--- wannago-server/src/main/java/com/wannago/qna/entity/Answer.java
+++ wannago-server/src/main/java/com/wannago/qna/entity/Answer.java
@@ -1,7 +1,9 @@
 package com.wannago.qna.entity;
 
+import com.wannago.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,6 +15,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity(name = "answer")
 @EntityListeners(AuditingEntityListener.class)
 public class Answer {
@@ -25,6 +28,10 @@ public class Answer {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ask_id", nullable = false)
     private Ask askId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member memberId;
 
     @Column(name = "author", length = 30, nullable = false)
     private String author;
@@ -42,4 +49,6 @@ public class Answer {
     @UpdateTimestamp
     @Column(name = "modified_date", columnDefinition = "TIMESTAMP", nullable = false)
     private LocalDateTime modifiedDate;
+
+
 }
