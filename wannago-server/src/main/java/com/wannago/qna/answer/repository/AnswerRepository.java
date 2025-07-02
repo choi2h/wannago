9648dac8d33466ebdc4bbc38393b959d8f8bb445
kang1979 <kangbyeongch1979@gmail.com>
2025-07-02 15:26:25 +0900

    feat/"answer-create: 답변 등록 repository 구현 "

---
+++ wannago-server/src/main/java/com/wannago/qna/answer/repository/AnswerRepository.java
@@ -0,0 +1,11 @@
+package com.wannago.qna.answer.repository;
+
+import com.wannago.qna.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+
+
+}
