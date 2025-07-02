5cdb5aa514fad1eb6d8e6617f67a257f5b475f57
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-07-02 15:19:41 +0900

    refactor: 댓글 유효성 검사 로직을 @Valid 기반으로 변경 #20

--- wannago-server/src/main/java/com/wannago/post/dto/CommentRequest.java
+++ wannago-server/src/main/java/com/wannago/post/dto/CommentRequest.java
@@ -1,5 +1,7 @@
 package com.wannago.post.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentRequest {
+    @NotBlank(message = "댓글 내용은 비어 있을 수 없습니다.")
+    @Size(max = 100, message = "댓글은 최대 100자까지 입력할 수 있습니다.")
     private String content;
 }
