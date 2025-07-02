2ad7baeea1d9998709bc731e35d06d2e39261b3b
ddolly518 <20211109@sungshin.ac.kr>
2025-07-02 16:02:12 +0900

    fix: port number

--- wannago-server/src/main/java/com/wannago/member/config/SecurityConfig.java
+++ wannago-server/src/main/java/com/wannago/member/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors.configurationSource(request -> {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트 도메인
+            configuration.setAllowedOrigins(List.of("http://localhost:5173")); // 프론트 도메인
             configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE")); // 허용 HTTP 메서드
             configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
             configuration.setAllowCredentials(true); // 인증정보 포함 허용
