89d04fa5f8b37b79a53d0cc089d713613b7f81dc
ddolly518 <20211109@sungshin.ac.kr>
2025-07-02 17:32:45 +0900

    fix: add annotation

--- wannago-server/src/main/java/com/wannago/member/jwt/CustomAuthenticationEntryPoint.java
+++ wannago-server/src/main/java/com/wannago/member/jwt/CustomAuthenticationEntryPoint.java
@@ -6,11 +6,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Map;
 
 // 전역 인증 실패 처리 클래스
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
