2de14ad33767eb05342b0bf5e6af083dc6057d7b
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-07-03 17:04:49 +0900

    chore: 대댓글 에러코드 추가 #20

--- wannago-server/src/main/java/com/wannago/common/exception/CustomErrorCode.java
+++ wannago-server/src/main/java/com/wannago/common/exception/CustomErrorCode.java
@@ -23,10 +23,10 @@ public enum CustomErrorCode {
 
     // 댓글 C
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "C001", "요청하신 댓글을 찾을 수 없습니다."),
-    COMMENT_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "C002", "댓글을 수정할 권한이 없습니다.");
+    COMMENT_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "C002", "댓글을 수정할 권한이 없습니다."),
 
     // 답글 R
-
+    INVALID_COMMENT_OPERATION(HttpStatus.BAD_REQUEST, "R001", "대댓글에는 다시 대댓글을 달 수 없습니다."),
    
 
 
