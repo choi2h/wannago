6928682bdb16cdf83579fe3d7b73feda150a0aa4
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-07-03 14:53:45 +0900

    fix: 문법 오류 수정

--- wannago-server/src/main/java/com/wannago/common/exception/CustomErrorCode.java
+++ wannago-server/src/main/java/com/wannago/common/exception/CustomErrorCode.java
@@ -19,15 +19,13 @@ public enum CustomErrorCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED,"M009", "유효하지 않은 토큰입니다."),
     
     // 게시글 P
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "요청하신 게시글을 찾을 수 없습니다.");
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "요청하신 게시글을 찾을 수 없습니다."),
 
     // 댓글 C
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "C001", "요청하신 댓글을 찾을 수 없습니다."),
+    COMMENT_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "C002", "댓글을 수정할 권한이 없습니다."),
 
-
-    // 답글 R
-
-    COMMENT_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "C002", "댓글을 수정할 권한이 없습니다.");
+    // 대댓글 R
 
 
     // 질문 Q
