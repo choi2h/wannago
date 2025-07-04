b7318579d533e9b8a981ca91789d211e3b6a66fa
kang1979 <kangbyeongch1979@gmail.com>
2025-07-04 09:42:59 +0900

    feat/"answer-list: 채택된 답변 우선 정렬 메소드 추가 "

--- wannago-server/src/main/java/com/wannago/qna/answer/repository/AnswerRepository.java
+++ wannago-server/src/main/java/com/wannago/qna/answer/repository/AnswerRepository.java
@@ -1,7 +1,15 @@
 package com.wannago.qna.answer.repository;
 
 import com.wannago.qna.entity.Answer;
+import com.wannago.qna.entity.Ask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    @Query("SELECT a FROM answer a WHERE a.askId = :ask ORDER BY a.isAccepted DESC, a.createdDate ASC")
+    List<Answer> findByAskIdOrderByIsAcceptedDesc(@Param("ask") Ask ask);
 }
