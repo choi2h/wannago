8019aa9e74192d779dc1f0e6464fc9eb9a25827d
kang1979 <kangbyeongch1979@gmail.com>
2025-07-04 09:45:23 +0900

    feat/"answer-list: 답변 조회 서비스 구현 "

--- wannago-server/src/main/java/com/wannago/qna/answer/service/AnswerService.java
+++ wannago-server/src/main/java/com/wannago/qna/answer/service/AnswerService.java
@@ -3,7 +3,10 @@ package com.wannago.qna.answer.service;
 import com.wannago.qna.answer.dto.AnswerRequest;
 import com.wannago.qna.answer.dto.AnswerResponse;
 
+import java.util.List;
+
 public interface AnswerService {
     AnswerResponse updateAnswer(Long answerId,String LoginId, AnswerRequest request);
     AnswerResponse createAnswer(Long askId, String LoginId,AnswerRequest request);
+    List<AnswerResponse> getAnswersByAskId(Long askId);
 }
