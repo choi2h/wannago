9ab59281ec62b553d8633af55afededc8bca5eeb
kang1979 <kangbyeongch1979@gmail.com>
2025-07-04 13:14:15 +0900

    feat/"answer-accept: 답변 채택 레파지토리 추가"

--- wannago-server/src/main/java/com/wannago/qna/answer/repository/AnswerRepository.java
+++ wannago-server/src/main/java/com/wannago/qna/answer/repository/AnswerRepository.java
@@ -1,7 +1,15 @@
 package com.wannago.qna.answer.repository;
 
 import com.wannago.qna.entity.Answer;
+import com.wannago.qna.entity.Ask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    // 특정 질문에 채택된 답변이 있는지 확인
+    boolean existsByAskIdAndIsAcceptedTrue(Ask ask);
+
+    // 특정 질문의 채택된 답변 조회
+    Optional<Answer> findByAskIdAndIsAcceptedTrue(Ask ask);
 }
