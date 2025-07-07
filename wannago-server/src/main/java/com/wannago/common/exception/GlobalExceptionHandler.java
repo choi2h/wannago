9539157ea88bf5441dde02a6731cd94676dae971
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-07-07 16:23:56 +0900

    feat: fix SecurityConfig, GlobalExceptionHandler 수정

--- wannago-server/src/main/java/com/wannago/common/exception/GlobalExceptionHandler.java
+++ wannago-server/src/main/java/com/wannago/common/exception/GlobalExceptionHandler.java
@@ -1,8 +1,7 @@
 package com.wannago.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import
-        org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
