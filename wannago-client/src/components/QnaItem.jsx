804792998754d7a0e33290cf6795804101da527b
choeihwa <chldlghk@gmail.com>
2025-07-08 01:16:31 +0900

    fix: 질문목록에서 채택여부 노출

--- wannago-client/src/components/QnaItem.jsx
+++ wannago-client/src/components/QnaItem.jsx
@@ -5,8 +5,8 @@ function QnaItem ({qna, onClick}) {
     <article className="qna-frame" onClick={() => onClick(qna.id)}> {/* div 대신 article 태그 사용 */}
       <header className="qna-header">
         <h2 className="qna-title">{qna.title}</h2> {/* 제목은 h2 태그로 */}
-        <div className={`qna-status ${qna.isAccepted ? "accepted" : "pending"}`}> {/* 상태에 따라 클래스 동적 할당 */}
-          {qna.isAccepted ? "채택완료" : "채택대기"}
+        <div className={`qna-status ${qna.accepted ? "accepted" : "pending"}`}> {/* 상태에 따라 클래스 동적 할당 */}
+          {qna.accepted ? "채택완료" : "채택대기"}
         </div>
       </header>
       <div className="qna-meta">
